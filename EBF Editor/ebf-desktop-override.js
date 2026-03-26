d1a92feba2978343b7c819ffa9845ff434dc0eb1
eselagas <ethanselagea72@outlook.com>
2026-03-26 12:31:07 +0000

    Update ebf-desktop-override.js

--- EBF Editor/ebf-desktop-override.js
+++ EBF Editor/ebf-desktop-override.js
@@ -1,18 +1,11 @@
-	/* Script for Desktop functionality replacement */
+	/* Modified script for Desktop functionality replacement */
+    const html = true;
 
 	function setTitle(title) {
 		document.title = title;
-		app_run("title", title);
 	}
 
 	function app_run(f, v) {
-		log("Running App Action");
-		if (html) {
-			log("Rejecting - standalone mode")
-		} else {
-	        if (f) {
-	            const payload = { function: f, data: v };
-	            window.chrome?.webview?.postMessage(JSON.stringify(payload));
-	        }
-        }
+		log("Rejected App Action");
+		return;
     }
